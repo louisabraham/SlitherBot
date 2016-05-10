59ffdc260caab336e0101c3bea62b390c554cb13
louisabraham <louis.abraham@yahoo.fr>
2016-05-11 00:28:51 +0200

    Syntax

--- Data.py
+++ Data.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 
-Snake = namedtuple('Snake',['id', 'nick', 'ang', 'head', 'body'])
-Point = namedtuple('Point',['x','y'])
-Food = namedtuple('Food', ['id', 'p', 'size'])
-Inputs = namedtuple('Inputs', ['left', 'right', 'acceleration'])
+Snake = namedtuple('Snake', 'id, nick, ang, head, body')
+Point = namedtuple('Point', 'x, y')
+Food = namedtuple('Food', 'id, p, size')
+Inputs = namedtuple('Inputs', 'left, right, acceleration')
